c8f7989a4cbf729d6cc7c98c51e45c1f962a0d7c
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-28 18:50:40 +0100

    Update NumberCard.java

--- src/My/NumberCard.java
+++ src/My/NumberCard.java
@@ -1,13 +1,14 @@
 package My;
 
-public class WildCard extends Card {
-    // Constructor for WildCard that only takes the action (Wild or WildFour)
-    public WildCard(Value value) {
-        super(CardColor.Wild, value); // WildCard is always of Wild color, value is Wild or WildFour
+public class NumberCard extends Card {
+    
+    // Constructor that takes both color and value
+    public NumberCard(CardColor color, Value value) {
+        super(color, value); // Call the parent constructor to set the color and value
     }
 
     @Override
     public String toString() {
-        return color + " - " + value; // String representation of the WildCard
+        return color + " - " + value; // String representation of the card
     }
 }
