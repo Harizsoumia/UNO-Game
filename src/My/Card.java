a82efcc8117398abcf35dddd1255037c6030b302
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-28 18:45:56 +0100

    Update Card.java

--- src/My/Card.java
+++ src/My/Card.java
@@ -2,28 +2,36 @@ package My;
 
 public abstract class Card {
     protected CardColor color;
+    protected Value value; // Add this field to store the value of the card
 
-    public Card(CardColor color) {
+    // Constructor that accepts both color and value
+    public Card(CardColor color, Value value) {
+        if (color == null || value == null) {
+            throw new IllegalArgumentException("Card color and value cannot be null");
+        }
         this.color = color;
+        this.value = value;  // Set the value of the card
     }
 
     public CardColor getColor() {
         return this.color;
     }
 
-    public abstract String getValue();
+    public Value getValue() {
+        return this.value; // Now we have access to the card's value
+    }
 
     @Override
-    public abstract String toString();
+    public abstract String toString(); // Abstract method for card representation
 
     // Enum for card colors
     public enum CardColor {
-        Red, Yellow, Green, Blue, Wild;
+        Red, Yellow, Green, Blue, Wild; // Wild as a special color
     }
 
     // Enum for card values
     public enum Value {
-        0, 1, 2, 3,4, 5, 6, 7, 8,9,
-        Draw, Skip, Reverce, WILD, WILD_FOUR;
+        Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine,
+        DrawTwo, Skip, Reverse, Wild, WildFour;
     }
 }
