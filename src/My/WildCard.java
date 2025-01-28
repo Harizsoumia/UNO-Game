75038f465023ff179df9d10ede2879724b8695da
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-28 18:51:29 +0100

    Update WildCard.java

--- src/My/WildCard.java
+++ src/My/WildCard.java
@@ -1,22 +1,13 @@
 package My;
 
-	public class WildCard extends Card {
-	    private final Value type;
+public class WildCard extends Card {
+    // Constructor for WildCard that only takes the action (Wild or WildFour)
+    public WildCard(Value value) {
+        super(CardColor.Wild, value); // WildCard is always of Wild color, value is Wild or WildFour
+    }
 
-	    public WildCard(Value type) {
-	        super(CardColor.WILD);
-	        this.type = type;
-	    }
-
-	    @Override
-	    public String getValue() {
-	        return type.toString();
-	    }
-
-	    @Override
-	    public String toString() {
-	        return type.toString();
-	    }
-	}
-
-
+    @Override
+    public String toString() {
+        return color + " - " + value; // String representation of the WildCard
+    }
+}
