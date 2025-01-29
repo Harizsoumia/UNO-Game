a9e3cd309aa03bc0c903efb81c03bcc0e5b2f384
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-29 23:17:37 +0100

    Update NumberCard.java

--- src/My/NumberCard.java
+++ src/My/NumberCard.java
@@ -1,14 +1,7 @@
 package My;
 
 public class NumberCard extends Card {
-    
-    // Constructor that takes both color and value
     public NumberCard(CardColor color, Value value) {
-        super(color, value); // Call the parent constructor to set the color and value
-    }
-
-    @Override
-    public String toString() {
-        return color + " - " + value; // String representation of the card
+        super(color, value);
     }
 }
