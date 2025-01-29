b1334a1ba8fefed58d3391f7573a521bdc51eba9
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-29 23:12:52 +0100

    Update Deck.java

--- src/My/Deck.java
+++ src/My/Deck.java
@@ -1,19 +1,19 @@
 package My;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Deck {
-    private List<Card> cards;
+    private Card[] cards;
+    private int topIndex;
 
     public Deck() {
-        cards = new ArrayList<>();
+        cards = new Card[108];
+        topIndex = 0;
         reset();
     }
 
     public void reset() {
-        cards.clear();
+        topIndex = 0;
         addNumberCards();
         addActionCards();
         addWildCards();
@@ -21,69 +21,54 @@ public class Deck {
     }
 
     private void addNumberCards() {
+        int index = 0;
         for (Card.CardColor color : Card.CardColor.values()) {
-            if (color != Card.CardColor.Wild) { // Skip Wild color
+            if (color != Card.CardColor.Wild) {
                 for (int i = 0; i <= 9; i++) {
-                    Card.Value value = Card.Value.values()[i]; // Convert the number to corresponding Value enum
-
-                    cards.add(new NumberCard(color, value));  // Add a number card
-
-                    if (i != 0) {  // Add a second card for numbers 1-9
-                        cards.add(new NumberCard(color, value));
+                    cards[index++] = new NumberCard(color, Card.Value.values()[i]);
+                    if (i != 0) {
+                        cards[index++] = new NumberCard(color, Card.Value.values()[i]);
                     }
                 }
             }
         }
     }
 
-
-
     private void addActionCards() {
+        int index = 76;
         for (Card.CardColor color : Card.CardColor.values()) {
-            if (color != Card.CardColor.Wild) { // Skip Wild color
+            if (color != Card.CardColor.Wild) {
                 for (Card.Value action : new Card.Value[]{Card.Value.DrawTwo, Card.Value.Skip, Card.Value.Reverse}) {
-                    cards.add(new ActionCard(color, action));  // Pass color and value (action)
-                    cards.add(new ActionCard(color, action));  // Add a second card for the action
+                    cards[index++] = new ActionCard(color, action);
+                    cards[index++] = new ActionCard(color, action);
                 }
             }
         }
     }
 
     private void addWildCards() {
+        int index = 100;
         for (Card.Value wild : new Card.Value[]{Card.Value.Wild, Card.Value.WildFour}) {
             for (int i = 0; i < 4; i++) {
-                cards.add(new WildCard(wild));
+                cards[index++] = new WildCard(Card.CardColor.Wild, wild);
             }
         }
     }
 
     public void shuffleDeck() {
-        Collections.shuffle(cards);
+        Random rand = new Random();
+        for (int i = cards.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            Card temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
     }
 
     public Card drawCard() {
-        if (cards.isEmpty()) {
+        if (topIndex >= cards.length) {
             throw new IllegalArgumentException("Cannot draw from an empty deck. Consider resetting the deck.");
         }
-        return cards.remove(cards.size() - 1);
-    }
-
-    public List<Card> drawCards(int num) {
-        if (num < 0 || num > cards.size()) {
-            throw new IllegalArgumentException("Invalid number of cards to draw: " + num);
-        }
-        List<Card> drawnCards = new ArrayList<>();
-        for (int i = 0; i < num; i++) {
-            drawnCards.add(drawCard());
-        }
-        return drawnCards;
-    }
-
-    public boolean isEmpty() {
-        return cards.isEmpty();
-    }
-
-    public int cardsRemaining() {
-        return cards.size();
+        return cards[topIndex++];
     }
 }
