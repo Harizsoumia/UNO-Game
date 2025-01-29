25bfea75d07c0f12e83a31ad5acb387095fa8628
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-29 22:49:30 +0100

    Update ActionCard.java

--- src/My/ActionCard.java
+++ src/My/ActionCard.java
@@ -1,13 +1,7 @@
 package My;
 
 public class ActionCard extends Card {
-    // Constructor for ActionCard that takes color and action (value)
     public ActionCard(CardColor color, Value value) {
-        super(color, value); // Call the parent constructor with color and value
-    }
-
-    @Override
-    public String toString() {
-        return color + " - " + value; // String representation of the ActionCard
+        super(color, value);
     }
 }
