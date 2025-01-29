0309b0eec416467fec1b735f9d003b02b9254697
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-29 23:16:16 +0100

    Update InvalidPlayerTurnException.java

--- src/My/InvalidPlayerTurnException.java
+++ src/My/InvalidPlayerTurnException.java
@@ -1,7 +1,7 @@
 package My;
 
 public class InvalidPlayerTurnException extends Exception {
-    private static final long serialVersionUID = 1L;  // Added serialVersionUID
+    private static final long serialVersionUID = 1L;
     private String playerId;
 
     public InvalidPlayerTurnException(String message, String playerId) {
