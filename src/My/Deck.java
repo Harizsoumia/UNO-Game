42e1f49561c77eda57d6bd718f7c2ff64aa1851d
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-01-29 23:13:49 +0100

    Update Deck.java

--- src/My/Deck.java
+++ src/My/Deck.java
@@ -2,73 +2,185 @@ package My;
 
 import java.util.*;
 
-public class Deck {
-    private Card[] cards;
-    private int topIndex;
+public class Game {
+    private Deck deck;
+    private List<Player> players;
+    private int currentPlayerIndex;
+    private Card topCard;
+    private boolean reverseDirection = false;
 
-    public Deck() {
-        cards = new Card[108];
-        topIndex = 0;
-        reset();
+    public Game() {
+        this.deck = new Deck();
+        this.players = new ArrayList<>();
+        this.currentPlayerIndex = 0;
+        this.topCard = deck.drawCard();
+        setupGame();  // Set up the game
+        dealInitialCards();
     }
 
-    public void reset() {
-        topIndex = 0;
-        addNumberCards();
-        addActionCards();
-        addWildCards();
-        shuffleDeck();
+
+
+    // Set up the game by asking for player preferences
+    private void setupGame() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for number of players
+        System.out.print("Enter number of players (2-4): ");
+        int numPlayers = scanner.nextInt();
+        while (numPlayers < 2 || numPlayers > 4) {
+            System.out.print("Invalid number! Enter number of players (2-4): ");
+            numPlayers = scanner.nextInt();
+        }
+
+        // Set up players
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.print("Enter name for Player " + (i + 1) + ": ");
+            String playerName = scanner.next();
+
+            System.out.print("Is " + playerName + " a human (yes/no)? ");
+            String isHuman = scanner.next();
+
+            if (isHuman.equalsIgnoreCase("yes")) {
+                players.add(new Player(playerName)); // Human player
+            } else {
+                players.add(new Bot(playerName)); // Bot player
+            }
+        }
     }
 
-    private void addNumberCards() {
-        int index = 0;
-        for (Card.CardColor color : Card.CardColor.values()) {
-            if (color != Card.CardColor.Wild) {
-                for (int i = 0; i <= 9; i++) {
-                    cards[index++] = new NumberCard(color, Card.Value.values()[i]);
-                    if (i != 0) {
-                        cards[index++] = new NumberCard(color, Card.Value.values()[i]);
+    // Deal initial cards to all players
+    private void dealInitialCards() {
+        for (Player player : players) {
+            for (int i = 0; i < 7; i++) {
+                player.addCard(deck.drawCard());
+            }
+        }
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            Player currentPlayer = players.get(currentPlayerIndex);
+            System.out.println("It's " + currentPlayer.getName() + "'s turn.");
+            System.out.println("Top card: " + topCard);
+            currentPlayer.showCards();
+
+            if (currentPlayer instanceof Bot) {
+                Card playedCard = ((Bot) currentPlayer).playCard(topCard);
+                if (playedCard != null) {
+                    topCard = playedCard;
+                    System.out.println(currentPlayer.getName() + " played: " + playedCard);
+                    handleSpecialCardEffects(playedCard, currentPlayer);
+                }
+            } else {
+                System.out.print("Enter card index to play (or -1 to draw): ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();  // Consume the leftover newline character
+
+                if (choice == -1) {
+                    Card drawnCard = deck.drawCard();
+                    currentPlayer.addCard(drawnCard);
+                    System.out.println(currentPlayer.getName() + " drew a card: " + drawnCard);
+                } else {
+                    try {
+                        Card playedCard = currentPlayer.playCard(choice);
+                        if (isValidMove(playedCard)) {
+                            topCard = playedCard;
+                            System.out.println(currentPlayer.getName() + " played: " + playedCard);
+                            handleSpecialCardEffects(playedCard, currentPlayer);
+                        } else {
+                            System.out.println("Invalid move! You can only play a card that matches the top card.");
+                            currentPlayer.addCard(playedCard);
+                        }
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
                     }
                 }
             }
+
+            if (currentPlayer.hasEmptyHand()) {
+                System.out.println(currentPlayer.getName() + " wins!");
+                break;
+            }
+
+            currentPlayerIndex = (currentPlayerIndex + (reverseDirection ? -1 : 1) + players.size()) % players.size();
         }
+
+        // Do not close the scanner here, as it will close System.in.
+        // scanner.close();
     }
 
-    private void addActionCards() {
-        int index = 76;
-        for (Card.CardColor color : Card.CardColor.values()) {
-            if (color != Card.CardColor.Wild) {
-                for (Card.Value action : new Card.Value[]{Card.Value.DrawTwo, Card.Value.Skip, Card.Value.Reverse}) {
-                    cards[index++] = new ActionCard(color, action);
-                    cards[index++] = new ActionCard(color, action);
-                }
+    private boolean isValidMove(Card card) {
+        return card.getColor() == topCard.getColor() || card.getValue() == topCard.getValue() || card instanceof WildCard || card instanceof WildFourCard;
+    }
+
+    private void handleSpecialCardEffects(Card playedCard, Player currentPlayer) {
+        if (playedCard instanceof ActionCard) {
+            ActionCard actionCard = (ActionCard) playedCard;
+            switch (actionCard.getValue()) {
+                case DrawTwo:
+                    System.out.println(currentPlayer.getName() + " played Draw Two! Next player must draw 2 cards.");
+                    nextPlayerDrawsTwo();
+                    break;
+                case Skip:
+                    System.out.println(currentPlayer.getName() + " played Skip! Next player is skipped.");
+                    skipNextPlayer();
+                    break;
+                case Reverse:
+                    reverseDirection = !reverseDirection;
+                    System.out.println(currentPlayer.getName() + " played Reverse! The turn order is now " + (reverseDirection ? "reversed." : "normal."));
+                    break;
+                default:
+                    // Number cards (0-9) do nothing special, so ignore them.
+                    break;
+            }
+        } else if (playedCard instanceof WildCard || playedCard instanceof WildFourCard) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(currentPlayer.getName() + " played a Wild card! Choose a color: (Red, Yellow, Green, Blue)");
+            String color = scanner.nextLine().toUpperCase();
+
+            try {
+                Card.CardColor chosenColor = Card.CardColor.valueOf(color);
+                topCard = new WildCard(chosenColor, playedCard.getValue());
+                System.out.println(currentPlayer.getName() + " changed the color to " + chosenColor + "!");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid color! Defaulting to Red.");
+                topCard = new WildCard(Card.CardColor.Red, playedCard.getValue());
+            }
+
+            if (playedCard instanceof WildFourCard) {
+                System.out.println(currentPlayer.getName() + " played Wild Draw Four! Next player must draw 4 cards.");
+                nextPlayerDrawsFour();
             }
         }
     }
 
-    private void addWildCards() {
-        int index = 100;
-        for (Card.Value wild : new Card.Value[]{Card.Value.Wild, Card.Value.WildFour}) {
-            for (int i = 0; i < 4; i++) {
-                cards[index++] = new WildCard(Card.CardColor.Wild, wild);
-            }
+
+
+    private void nextPlayerDrawsTwo() {
+        int nextPlayerIndex = (currentPlayerIndex + (reverseDirection ? -1 : 1) + players.size()) % players.size();
+        Player nextPlayer = players.get(nextPlayerIndex);
+        for (int i = 0; i < 2; i++) {
+            Card drawnCard = deck.drawCard();
+            nextPlayer.addCard(drawnCard);
+            System.out.println(nextPlayer.getName() + " drew a card: " + drawnCard);
         }
     }
 
-    public void shuffleDeck() {
-        Random rand = new Random();
-        for (int i = cards.length - 1; i > 0; i--) {
-            int j = rand.nextInt(i + 1);
-            Card temp = cards[i];
-            cards[i] = cards[j];
-            cards[j] = temp;
+    private void skipNextPlayer() {
+        currentPlayerIndex = (currentPlayerIndex + (reverseDirection ? -1 : 1) + players.size()) % players.size();
+        System.out.println("Skipping next player.");
+    }
+    private void nextPlayerDrawsFour() {
+        int nextPlayerIndex = (currentPlayerIndex + (reverseDirection ? -1 : 1) + players.size()) % players.size();
+        Player nextPlayer = players.get(nextPlayerIndex);
+        System.out.println(nextPlayer.getName() + " must draw 4 cards!");
+        for (int i = 0; i < 4; i++) {
+            Card drawnCard = deck.drawCard();
+            nextPlayer.addCard(drawnCard);
+            System.out.println(nextPlayer.getName() + " drew a card: " + drawnCard);
         }
     }
 
-    public Card drawCard() {
-        if (topIndex >= cards.length) {
-            throw new IllegalArgumentException("Cannot draw from an empty deck. Consider resetting the deck.");
-        }
-        return cards[topIndex++];
-    }
 }
