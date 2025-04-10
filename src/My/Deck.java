6887e40d3bb401fdc505016411256608edc6bb48
Manel Nadir <manelnad018@gmail.com>
2025-04-10 15:56:21 +0100

    Update Deck.java

--- src/My/Deck.java
+++ src/My/Deck.java
@@ -1,74 +1,61 @@
-package My;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
-    private Card[] cards;
-    private int topIndex;
+    private List<Card> cards;
 
     public Deck() {
-        cards = new Card[108];
-        topIndex = 0;
-        reset();
-    }
-
-    public void reset() {
-        topIndex = 0;
-        addNumberCards();
-        addActionCards();
-        addWildCards();
+        cards = new ArrayList<>();
+        createDeck();
         shuffleDeck();
     }
 
-    private void addNumberCards() {
-        int index = 0;
-        for (Card.CardColor color : Card.CardColor.values()) {
-            if (color != Card.CardColor.Wild) {
+    private void createDeck() {
+        // Create number cards
+        for (Card.Color color : Card.Color.values()) {
+            if (color != Card.Color.Wild) {
                 for (int i = 0; i <= 9; i++) {
-                    cards[index++] = new NumberCard(color, Card.Value.values()[i]);
+                    cards.add(new NumberCard(color, i));
                     if (i != 0) {
-                        cards[index++] = new NumberCard(color, Card.Value.values()[i]);
+                        cards.add(new NumberCard(color, i)); // Add second instance for 1-9
                     }
                 }
             }
         }
-    }
 
-    private void addActionCards() {
-        int index = 76;
-        for (Card.CardColor color : Card.CardColor.values()) {
-            if (color != Card.CardColor.Wild) {
-                for (Card.Value action : new Card.Value[]{Card.Value.DrawTwo, Card.Value.Skip, Card.Value.Reverse}) {
-                    cards[index++] = new ActionCard(color, action);
-                    cards[index++] = new ActionCard(color, action);
-                }
+        // Create action cards
+        for (Card.Color color : Card.Color.values()) {
+            if (color != Card.Color.Wild) {
+                cards.add(new SkipCard(color)); // Assuming SkipCard constructor takes only color
+                cards.add(new SkipCard(color)); // Second instance
+                cards.add(new TakeTwoCard(color)); // Assuming TakeTwoCard constructor takes only color
+                cards.add(new TakeTwoCard(color)); // Second instance
+                cards.add(new ReverseCard(color)); // Assuming ReverseCard constructor takes only color
+                cards.add(new ReverseCard(color)); // Second instance
             }
         }
-    }
 
-    private void addWildCards() {
-        int index = 100;
-        for (Card.Value wild : new Card.Value[]{Card.Value.Wild, Card.Value.WildFour}) {
-            for (int i = 0; i < 4; i++) {
-                cards[index++] = new WildCard(Card.CardColor.Wild, wild);
-            }
+        // Create wild cards
+        for (int i = 0; i < 4; i++) {
+            cards.add(new WildCard()); // Assuming WildCard has a default constructor
+            cards.add(new WildrawFourCard()); // Assuming WildDrawFourCard has a default constructor
+            cards.add(new WildReverseCard()); // Assuming WildReverseCard has a default constructor
         }
     }
 
-    public void shuffleDeck() {
-        Random rand = new Random();
-        for (int i = cards.length - 1; i > 0; i--) {
-            int j = rand.nextInt(i + 1);
-            Card temp = cards[i];
-            cards[i] = cards[j];
-            cards[j] = temp;
-        }
+    private void shuffleDeck() {
+        Collections.shuffle(cards);
     }
 
     public Card drawCard() {
-        if (topIndex >= cards.length) {
-            throw new IllegalArgumentException("Cannot draw from an empty deck. Consider resetting the deck.");
+        if (cards.isEmpty()) {
+            throw new IllegalArgumentException("Cannot draw from an empty deck.");
         }
-        return cards[topIndex++];
+        return cards.remove(cards.size() - 1); // Draw from the top of the deck
+    }
+
+    public List<Card> getCards() {
+        return cards;
     }
 }
