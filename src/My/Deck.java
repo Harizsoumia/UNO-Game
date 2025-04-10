06110ba2aff560b6f4db92b2d190dd02d1eaf5fa
Manel Nadir <manelnad018@gmail.com>
2025-04-10 16:17:55 +0100

    Update Deck.java

--- src/My/Deck.java
+++ src/My/Deck.java
@@ -55,7 +55,29 @@ public class Deck {
         return cards.remove(cards.size() - 1); // Draw from the top of the deck
     }
 
+    public void resetDeck() {
+    cards.clear();
+    createDeck();
+    shuffleDeck();
+}
+
     public List<Card> getCards() {
         return cards;
     }
+
+    //A method to peek at the top card without removing it from the deck
+    public Card peekTopCard() {
+    if (cards.isEmpty()) {
+        throw new IllegalArgumentException("Cannot peek at an empty deck.");
+    }
+    return cards.get(cards.size() - 1);
+}
+    //method to return a size of the deck
+    public int size() {
+    return cards.size();
+}
+    //A method to add a card back to the deck. This can be useful if you want to implement a game mechanic where cards can be returned to the deck.
+    public void addCard(Card card) {
+    cards.add(card);
+}
 }
